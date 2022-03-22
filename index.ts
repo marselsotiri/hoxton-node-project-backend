6efa334a2686349f807d48ebf97db6d8b18505dd
Marsel <you@example.com>
2022-03-22 22:46:12 +0100

    commit

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
 const app = express()
 app.use(cors())
@@ -13,8 +14,74 @@ const prisma = new PrismaClient({
   log: ['error', 'info', 'query', 'warn']
 })
 
+function createToken(id: number) {
+  // @ts-ignore
+  return jwt.sign({ id: id }, process.env.MY_SECRET, { expiresIn: '3days' })
+}
+
+async function getUserFromToken(token: string) {
+  // @ts-ignore
+  const decodedData = jwt.verify(token, process.env.MY_SECRET)
+  const user = await prisma.user.findUnique({
+    // @ts-ignore
+    where: { id: decodedData.id },
+    include: { users: true }
+  })
+
+  return user
+}
+
+app.post('/sign-up', async (req, res) => {
+  const { email, password, fullName, phoneNr, profilePhoto, userStatus } = req.body
+
+  try {
+    const hash = bcrypt.hashSync(password, 8)
+    const user = await prisma.user.create({
+      //@ts-ignore
+      data: { email: email, password: hash, fullName: fullName, phoneNr: phoneNr, profilePhoto: profilePhoto, userStatus: userStatus }
+    })
+    res.send({ user, token: createToken(user.id) })
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message })
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password, phoneNr } = req.body
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email }
+    })
+    // @ts-ignore
+    const passwordMatches = bcrypt.compareSync(password, user.password)
+
+    if (user && passwordMatches) {
+      res.send({ user, token: createToken(user.id) })
+    } else {
+      throw Error('BOOM!')
+    }
+  } catch (err) {
+    res.status(400).send({ error: 'User/password invalid.' })
+  }
+})
+
+app.get('/validate', async (req, res) => {
+  const token = req.headers.authorization || ''
+
+  try {
+    // @ts-ignore
+    const user = await getUserFromToken(token)
+    res.send(user)
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message })
+  }
+})
+
 
 
 app.listen(4000, () => {
-    console.log(`Server up: http://localhost:4000`)
-  })
+  console.log(`Server up: http://localhost:4000`)
+})
